7f72a1ed1fe47a20b0b73c3013d2caedcf8dd07f
Palash Bansal <PalashBansal96@gmail.com>
2015-11-14 17:01:35 +0530

    IntelliJ Import and remove public from interface

--- src/com/iiitd/ap/lab10/Observer.java
+++ src/com/iiitd/ap/lab10/Observer.java
@@ -1,5 +1,5 @@
 package com.iiitd.ap.lab10;
 
 public interface Observer {
-	public void update();
+	void update();
 }
--- src/com/iiitd/ap/lab10/Subject.java
+++ src/com/iiitd/ap/lab10/Subject.java
@@ -1,7 +1,7 @@
 package com.iiitd.ap.lab10;
 
 public interface Subject{
-	public void registerObserver(Observer observer);
-	public void removeObserver(Observer observer);
-	public void notifyObservers();
+	void registerObserver(Observer observer);
+	void removeObserver(Observer observer);
+	void notifyObservers();
 }
