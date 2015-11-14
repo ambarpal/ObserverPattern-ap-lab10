f72a26271fc56f3a4716aafb79075677463eceab
Palash Bansal <PalashBansal96@gmail.com>
2015-11-14 18:33:54 +0530

    Location temperature updated using threads

--- src/com/iiitd/ap/lab10/TemperatureSensor.java
+++ src/com/iiitd/ap/lab10/TemperatureSensor.java
@@ -5,18 +5,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class TemperatureSensor implements Subject{
-	private List<Observer> observers = Collections.synchronizedList(new ArrayList<Observer>());
-	private ArrayList<TemperatureLog> temps = new ArrayList<TemperatureLog>();
+	private List<Observer> observers = Collections.synchronizedList(new ArrayList<>());
+	private ArrayList<TemperatureGenerator> updaters = new ArrayList<>();
+	private TemperatureLog lastUpdate;
 	RandomNumberGenerator r = new RandomNumberGenerator();
 	TemperatureSensor(){
-		temps.add(new TemperatureLog("Home", 35.00));
-		temps.add(new TemperatureLog("Dog's Home", 35.00));
-		temps.add(new TemperatureLog("Mausi's Home", 35.00));
+		updaters.add(new TemperatureGenerator(new TemperatureLog("Delhi", 35.0)).startThread());
+		updaters.add( new TemperatureGenerator( new TemperatureLog("Mumbai", 35.0) ).startThread());
+		updaters.add(new TemperatureGenerator(new TemperatureLog("Shrinagar", 35.0)).startThread());
+
 		notifyObservers();
 	}
 	public void senseTemperatures(){
-		for (TemperatureLog t : temps) t.setTemperature(r.next());
 		notifyAll();
+	}
+	public void addLocation(String name){
+
 	}
 	@Override
 	synchronized public void registerObserver(Observer observer){
@@ -30,11 +34,45 @@ public class TemperatureSensor implements Subject{
 	
 	@Override
 	synchronized public void notifyObservers(){
-		for (Observer o : observers) o.update();
+		observers.forEach(Observer::update);
 	}
 	
 	@Override
 	synchronized public Object getUpdate(){
-		return temps;
+		return lastUpdate;
+	}
+
+	private class TemperatureGenerator extends Thread{
+		TemperatureLog temperatureLog;
+		private boolean isWorking;
+
+		public TemperatureGenerator(TemperatureLog log){
+			temperatureLog = log;
+		}
+		public TemperatureGenerator startThread(){
+			isWorking = true;
+			start();
+			return this;
+		}
+		@Override
+		public void run() {
+			while(isWorking){
+				updateTemperature();
+				notifyObservers();
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException ignored) {
+				}
+			}
+		}
+
+		public void stopUpdating(){
+			isWorking = false;
+		}
+
+		synchronized private void updateTemperature() {
+			temperatureLog.setTemperature(r.next());
+			lastUpdate = this.temperatureLog;
+		}
 	}
 }
