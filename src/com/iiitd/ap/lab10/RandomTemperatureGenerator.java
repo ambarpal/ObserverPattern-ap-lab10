83ac1ff1c23afc48e5c0581fc5a46c1ca4b0818d
Palash Bansal <PalashBansal96@gmail.com>
2015-11-15 07:31:37 +0530

    RandomTemperature Generator fixed integer division bug

--- src/com/iiitd/ap/lab10/RandomTemperatureGenerator.java
+++ src/com/iiitd/ap/lab10/RandomTemperatureGenerator.java
@@ -10,6 +10,6 @@ public class RandomTemperatureGenerator {
 	public double next(){
 		// Returns double between -30 and 60. with accuracy of 10^-3
 		seed = 8253729 * seed + 2396403;
-		return (seed%90000)/1000 - 30;
+		return (seed%90000)/1000.0d - 30;
 	}
 }
