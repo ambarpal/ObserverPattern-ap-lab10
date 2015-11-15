478996443092a3846244f0b834ab7cb014d454df
Palash Bansal <PalashBansal96@gmail.com>
2015-11-15 06:05:57 +0530

    Implemented TemperaturePredictor1

--- src/com/iiitd/ap/lab10/TemperaturePredictor1.java
+++ src/com/iiitd/ap/lab10/TemperaturePredictor1.java
@@ -1,9 +1,12 @@
 package com.iiitd.ap.lab10;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Queue;
 
 public class TemperaturePredictor1 implements Observer{
 	Subject subject;
+	HashMap<String, Queue<Double>> pastTemperatures = new HashMap<>();
 	@Override
 	public void setSubject(Subject subject){
 		this.subject = subject;
@@ -11,6 +14,15 @@ public class TemperaturePredictor1 implements Observer{
 	
 	@Override
 	public void update(){
-		ArrayList<TemperatureLog> a = (ArrayList<TemperatureLog>)subject.getUpdate();
+		ArrayList<TemperatureLog> temperatureLogs = (ArrayList<TemperatureLog>) subject.getUpdate();
+		for (TemperatureLog temperatureLog : temperatureLogs) {
+			Queue<Double> past = pastTemperatures.get(temperatureLog.getLocation());
+			past.poll();
+			past.offer(temperatureLog.getTemperature());
+			Double average = 0.0;
+			for(Double d : past) average += d;
+			average/=past.size();
+			System.out.println("Temperature Predictor 1: Temperature prediction for " + temperatureLog.getLocation() + ": " + average + " degrees.");
+		}
 	}
 }
