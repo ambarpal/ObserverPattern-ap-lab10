cbb0bcac2f288c0646bac1434ef0c1111ac42bb8
Palash Bansal <PalashBansal96@gmail.com>
2015-11-15 06:14:12 +0530

    Implemented SerializeTemperature

--- src/com/iiitd/ap/lab10/SerializeTemperature.java
+++ src/com/iiitd/ap/lab10/SerializeTemperature.java
@@ -1,5 +1,8 @@
 package com.iiitd.ap.lab10;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class SerializeTemperature implements Observer{
@@ -11,6 +14,17 @@ public class SerializeTemperature implements Observer{
 	
 	@Override
 	public void update(){
-		ArrayList<TemperatureLog> a = (ArrayList<TemperatureLog>)subject.getUpdate();
+		ArrayList<TemperatureLog> temperatureLogs = (ArrayList<TemperatureLog>) subject.getUpdate();
+		for (TemperatureLog temperatureLog : temperatureLogs) {
+			try {
+				ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(temperatureLog.getLocation()+temperatureLog.getTimestamp()+".dat"));
+				out.writeObject(temperatureLog);
+				out.close();
+				System.out.println("Temperature Serializer: Temperature serialized for " + temperatureLog.getLocation() + " with timestamp " + temperatureLog.getTimestamp());
+			} catch (IOException e) {
+				System.out.println("Temperature Serializer: Error Serializing " + temperatureLog.getLocation() + " with timestamp " + temperatureLog.getTimestamp());
+				e.printStackTrace();
+			}
+		}
 	}
 }
