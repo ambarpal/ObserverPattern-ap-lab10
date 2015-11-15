de9a703688a1c72b39677e087be2a9d14ef7ac2a
Palash Bansal <PalashBansal96@gmail.com>
2015-11-15 06:35:20 +0530

    Fixed TemperaturePredictors

--- src/com/iiitd/ap/lab10/TemperaturePredictor2.java
+++ src/com/iiitd/ap/lab10/TemperaturePredictor2.java
@@ -2,12 +2,13 @@ package com.iiitd.ap.lab10;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class TemperaturePredictor2 implements Observer{
 	private static final int MAXLIMIT = 5;
 	Subject subject;
-	HashMap<String, Queue<Double>> pastTemperatures = new HashMap<>();
+	private HashMap<String, Queue<Double>> pastTemperatures = new HashMap<>();
 	@Override
 	public void setSubject(Subject subject){
 		this.subject = subject;
@@ -17,6 +18,7 @@ public class TemperaturePredictor2 implements Observer{
 	public void update(){
 		ArrayList<TemperatureLog> temperatureLogs = (ArrayList<TemperatureLog>) subject.getUpdate();
 		for (TemperatureLog temperatureLog : temperatureLogs) {
+			pastTemperatures.putIfAbsent(temperatureLog.getLocation(), new LinkedList<>());
 			Queue<Double> past = pastTemperatures.get(temperatureLog.getLocation());
 			if(past.size()==MAXLIMIT) past.poll();
 			past.offer(temperatureLog.getTemperature());
